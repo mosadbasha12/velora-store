58be74100e3d0285c49fa59547bffcf2d11cbfa3
mosadbasha12 <mohamedmosad1412@icloud.com>
2026-08-23 11:26:01 +0300

    Connect Velora to Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Local/deployment configuration. Replace the placeholders with your Firebase
 // Web App configuration. This file is intentionally not a password store.
 window.VELORA_FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA-XQFzHDNX-6jwdKOkVO5Q_VkpyoUS_po",
+  authDomain: "velora-store-6de42.firebaseapp.com",
+  projectId: "velora-store-6de42",
+  storageBucket: "velora-store-6de42.firebasestorage.app",
+  messagingSenderId: "340834760693",
+  appId: "1:340834760693:web:746f05d5b4fe5537ba92bc"
 };
-window.VELORA_OWNER_EMAIL = "YOUR_OWNER_EMAIL";
+window.VELORA_OWNER_EMAIL = "2001anamedo2001@gmail.com";
